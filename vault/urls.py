95991406be5b4a74f18b5808771e9e8d7c302f72
loedetyler <loedetyler@gmail.com>
2025-12-03 14:27:32 -0600

    Added form for password adding

--- vault/urls.py
+++ vault/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
     path('', views.vault_list),
+    path('add_password', add_password, name='add_password'),
 ]
--- vault/views.py
+++ vault/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Pass
 
 def vault_list(request):
     return render(request, 'vault/vault_list.html')
+
+def add_password(request):
+    if request.method == 'POST':
+        site = request.POST.get('site')
+        user = request.POST.get('username')
+        newpass = request.POST.get('password')
+
+        entry = Pass(username=user, site=site, password=newpass)
+        entry.save()
+
+        return HttpResponse("Account successfully added.")
+    else:
+        return HttpResponse("Invalid request method")
